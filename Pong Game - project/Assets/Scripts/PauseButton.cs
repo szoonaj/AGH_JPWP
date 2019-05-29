ebc0f27368b1f9004090e5d189f15a6408b813e9
szoonaj <szonaj98@gmail.com>
2019-05-29 20:44:27 +0200

    adding exercises

---
+++ Pong Game - project/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    /*
+        Uzupelnij dwie funkcje ponizej
+      
+        Time.timeScale = 0 >> zapauzowanie
+        Time.timeScale = 1 >> wznowienie
+        Input.GetKeyDown(KeyCode.X) >> użytkownik nacisnął X na klawiaturze
+
+    */
+    /*
+      
+     // pauza za pomoca buttonu
+
+    public void Pause()
+    {
+        if (...==...)
+        {
+            Time.timeScale = ...;
+        }
+        else if (...==...)
+        {
+            Time.timeScale = ...; 
+        }
+    }
+    */
+    bool isPaused = false;
+    /*
+      
+    // pauza klawiszem spacji
+    
+    void Update()
+    {
+        if (...)
+        {
+
+            if (isPaused == ...)
+            {
+
+                isPaused = ...;
+                Time.timeScale = ...;
+            }
+
+            else if (isPaused == ...)
+            {
+
+                isPaused = ...;
+                Time.timeScale = ...;
+            }
+
+        }
+    }
+    */
+}
---
+++ Pong Game - project/Assets/Scripts/Racket2AI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket2AI : MonoBehaviour
+{
+    /*
+        
+        GetComponent<Rigidbody2D>().velocity = new Vector2(wspolrz1,wspolrz2) * predkosc;
+        >> nadawanie kierunku i rozpoczecie ruchu
+
+    */
+    /*
+    public float movementSpeed;
+
+    public GameObject ball;
+
+    private void FixedUpdate()
+    {
+        if (Mathf.Abs(this.transform.position.y - ball.transform.position.y) > 10)      // mniejsza liczba >> AI szybciej zaczyna reagowac
+        {
+            if (transform.position.y < ball.transform.position.y)
+                // ...
+            else
+                // ...
+        }
+        else
+            // ...
+
+    }
+    */
+}
